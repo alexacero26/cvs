654ebd3867422002e59f2914f98e46f3dce89a4e
alexacero26 <76714865+alexacero26@users.noreply.github.com>
2023-12-24 14:30:43 -0500

    Update app.py
    cors

--- csvpydo/app.py
+++ csvpydo/app.py
@@ -2,8 +2,10 @@ from flask import Flask, request, jsonify, send_file
 import pandas as pd
 import os
 import uuid
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/convert', methods=['POST'])
 def json_to_csv():
@@ -75,3 +77,4 @@ def delete(file_path):
     
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
